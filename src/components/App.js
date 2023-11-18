0948aeb764bcd052e29d5cf7cc2919808ad93645
joy Kurui <kurui.chepngetich@gmail.com>
2023-11-18 16:03:24 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch} from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
@@ -7,7 +7,21 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return( 
+  <div>
+   <NavBar />
+   <Switch>
+    <Route exact path= "/" element={<Home />}><Home />
+    </Route>
+    <Route path= "/directors" element={<Directors />}><Directors />
+    </Route>
+    <Route path= "/actors" element={<Actors />}><Actors />
+    </Route>
+    <Route path= "/movies" element={<Movies />}><Movies />
+    </Route>
+   </Switch>
+  </div>
+  );
 }
 
 export default App;
